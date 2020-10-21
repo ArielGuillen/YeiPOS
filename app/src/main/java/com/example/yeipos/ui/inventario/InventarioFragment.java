7bfb5b1a9c3a5ced3cbf23cf0753ac057411993d
axel-flores <axel.flores.tonacatl@gmail.com>
2020-10-21 08:37:38 -0500

    Commint 4 by Axel Se termino de añadir las funciones de eliminar, actualizar y añadir

--- app/src/main/java/com/example/yeipos/ui/inventario/InventarioFragment.java
+++ app/src/main/java/com/example/yeipos/ui/inventario/InventarioFragment.java
@@ -5,35 +5,27 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.yeipos.AddToInventario;
-import com.example.yeipos.AgregarOrden;
-import com.example.yeipos.Inventario;
+import com.example.yeipos.ModificarInventario;
 import com.example.yeipos.R;
 import com.example.yeipos.interfaces.ItemClickListener;
 import com.example.yeipos.model.CardItem;
 import com.example.yeipos.model.ItemInventario;
-import com.example.yeipos.model.Producto;
 import com.example.yeipos.viewHolder.InventarioCardAdapter;
 import com.example.yeipos.viewHolder.InventarioCardAdapterV2;
 import com.firebase.ui.database.FirebaseRecyclerAdapter;
 import com.firebase.ui.database.FirebaseRecyclerOptions;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
 
@@ -157,6 +149,15 @@ public class InventarioFragment extends Fragment implements ItemClickListener {
                                 String id = model.getId();
                                 inventarioRef.child("inventario").child(id).removeValue();
                                 Toast.makeText(getActivity(),"Producto eliminado",Toast.LENGTH_SHORT).show();
+                            }
+                        });
+                        holder.mEditImage.setOnClickListener(new View.OnClickListener() {
+                            @Override
+                            public void onClick(View view) {
+                                String id = model.getId();
+                                Intent intent = new Intent(getActivity(), ModificarInventario.class);
+                                intent.putExtra("message", id);
+                                startActivity(intent);
                             }
                         });
                     }
